0bf83f10c1b8d6eadab9bc5f13c40147e954a17f
sefluegel <fluegel.scott@gmail.com>
2025-08-04 11:56:16 -0400

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Pro IRP MVP Dashboard</h1>
+      <p>Welcome to the churn-prediction dashboard.</p>
+    </div>
+  );
+}
+
+export default App;
