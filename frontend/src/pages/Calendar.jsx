2e48db686bf81503ce52d9b391ac2245cc5b64a8
sefluegel <fluegel.scott@gmail.com>
2025-08-07 22:04:29 -0400

    Update Calendar.jsx

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,9 +1,7 @@
+// /frontend/src/pages/Calendar.jsx
 import React, { useState } from "react";
 import { Calendar as BigCalendar, dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
+import { format, parse, startOfWeek, getDay } from "date-fns";
 import enUS from "date-fns/locale/en-US";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
