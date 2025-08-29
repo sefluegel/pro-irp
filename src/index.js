d8db50262f05cfa159c4b49cc0ffe5128931693c
sefluegel <fluegel.scott@gmail.com>
2025-08-29 13:19:30 -0400

    revert: remove injected React badge block (keeping external status script)

--- src/index.js
+++ src/index.js
@@ -11,21 +11,8 @@ root.render(
   </React.StrictMode>
 );
 
-const __apiBadgeContainer = document.createElement('div');
-__apiBadgeContainer.id = 'api-status-badge-root';
-__apiBadgeContainer.style.position = 'fixed';
-__apiBadgeContainer.style.top = '10px';
-__apiBadgeContainer.style.right = '10px';
-__apiBadgeContainer.style.zIndex = '9999';
-document.body.appendChild(__apiBadgeContainer);
-
-// Support React 18 createRoot or legacy render
-let __reactDom:any;
-try { __reactDom = require('react-dom/client'); } catch { __reactDom = null; }
-if (__reactDom && __reactDom.createRoot) {
-  const r = __reactDom.createRoot(__apiBadgeContainer);
-  r.render(React.createElement(ApiStatusDot, {}));
-} else {
+else {
   const ReactDOM = require('react-dom');
   ReactDOM.render(React.createElement(ApiStatusDot, {}), __apiBadgeContainer);
 }
+
