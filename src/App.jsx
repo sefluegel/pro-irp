a02b0a5c1fda9922efed8414a41a76480647aaa6
sefluegel <fluegel.scott@gmail.com>
2025-09-03 11:25:53 -0400

    routing: disable marketing on /; keep /login and /dashboard only

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 ﻿import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import HomeSimple from "./pages/marketing/HomeSimple";
+// DO NOT import HomeSimple for now
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomeSimple />} />
+      {/* redirect root to login for now */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
+
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+
+      {/* you can keep signup pointing to Login for now */}
       <Route path="/signup" element={<Login />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
+
+      {/* catch-all */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
